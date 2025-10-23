cb413631a36fe29ff6bcc304a3fa3ca0bf0434e5
Henrique O. <lemonheadch@outlook.com>
2025-10-23 20:51:18 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // optional, Vite uses 'dist' by default
+    outDir: 'dist'
   }
 })
